5df4f06a2469e7a6eec92a8d22a383f7d8ad0087
Marquet Reid <you@example.com>
2018-09-03 23:56:38 -0400

    cleaned up code

--- week-1/problem_2.py
+++ week-1/problem_2.py
@@ -18,30 +18,4 @@ def hey_bob(string):
     # print(f'Number of times bob occurs is: {count}')
     print('Number of times bob occurs is: ' + str(count))
 
-# def hey_bob(string):
-#     count = 0
-#     for i in range(len(string)):
-#         if(string[i:i + 3] == 'bob'):
-#             count += 1
-#     print('Number of times bob occurs is: ' + str(count)
-
-# def hey_bob(string):
-#     count = string.count('bob')
-#     if count > 0:
-#         print(f'bob is here {count} times')
-
-# def hey_bob(string):
-#     count = 0
-#     for i in range(len(string)):
-#         # print(f'{string[i]}')
-#         if (string[i] == 'b'):
-#             print(f'the first joint {string[i]}')
-#             if (int(string[i + 1]) <= len(string) and string[i + 1] == 'o'):
-#                 print(f'the second joint {string[i + 1]}')
-#                 if (i + 2 <= len(string) and string[i + 2] == 'b'):
-#                     print(f'the third joint {string[i + 2]}')  
-#                     count += 1
-#     print(f'the count is {count}')
-
-
 hey_bob(s)
